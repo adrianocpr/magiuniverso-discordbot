72813b811ac39691be10a78a0735e4aac849fab6
adrianocpr <47866496+adrianocpr@users.noreply.github.com>
2025-05-19 10:52:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
-
-if not TOKEN:
-    raise ValueError("DISCORD_TOKEN não está definido. Verifique suas variáveis de ambiente.")
 
 intents = discord.Intents.default()
+intents.message_content = True  # Adiciona permissão para ler conteúdo das mensagens
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot conectado como {bot.user}")
+    print(f'Bot conectado como {bot.user}')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
 
+TOKEN = os.getenv("DISCORD_TOKEN")
 bot.run(TOKEN)
