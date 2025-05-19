78de7de02d767295da35409474c18bee449bdf8c
adrianocpr <47866496+adrianocpr@users.noreply.github.com>
2025-05-19 10:09:43 -0300

    Add files via upload

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,3 @@
-DISCORD_BOT_TOKEN = "MTM3Mzk5NjM0MTUyNDY5NzEyMQ.GuYYKL.bLY34vspTLcZ7K2wbq8kF2NP7C1F02m0jrdKcI"
+DISCORD_BOT_TOKEN = "your_token_here"
 ADMIN_USER_IDS = [1234567890]  # Lista de IDs de administradores
 GITHUB_REPO_URL = "https://github.com/adrianocpr/Magiuniverso"
--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-import os
-import discord
-from discord.ext import commands
-from dotenv import load_dotenv
+from bot.core import bot
+from bot.config import DISCORD_BOT_TOKEN
 
-load_dotenv()
-TOKEN = os.getenv("MTM3Mzk5NjM0MTUyNDY5NzEyMQ.GuYYKL.bLY34vspTLcZ7K2wbq8kF2NP7C1F02m0jrdKcI")
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"Bot conectado como {bot.user}")
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong!")
-
-bot.run(TOKEN)
+bot.run(DISCORD_BOT_TOKEN)
