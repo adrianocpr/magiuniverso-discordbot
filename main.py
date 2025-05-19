e449b7b97aff130f4ac8d645f252f732ed8528ba
adrianocpr <47866496+adrianocpr@users.noreply.github.com>
2025-05-19 09:51:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("MTM3Mzk5NjM0MTUyNDY5NzEyMQ.GG--nl.VZ42UWV1SG96hEFti4_jnHfrOJe5WXRRLU3zTI")
+TOKEN = os.getenv("MTM3Mzk5NjM0MTUyNDY5NzEyMQ.GuYYKL.bLY34vspTLcZ7K2wbq8kF2NP7C1F02m0jrdKcI")
 
 intents = discord.Intents.default()
 intents.message_content = True
